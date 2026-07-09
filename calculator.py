50e81e1ba7c9beff932635053449c330c8697452
EmirhanKARAKOCSME <emirhankarakoc155@gmail.com>
2026-07-09 14:56:34 +0300

    init: calculator project and type casting

---
+++ calculator.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 30.5
+
+# I have mastered type conversion to robustly handle edge cases, making my calculator project significantly more resilient
+
+
+number2 = int(number2)
+
+print(number2+ number1)
