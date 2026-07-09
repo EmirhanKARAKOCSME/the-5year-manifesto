d25f45813cd6749f64c77006154fc3ef9d9b189f
EmirhanKARAKOCSME <emirhankarakoc155@gmail.com>
2026-07-09 16:43:22 +0300

    exception-handled-calculator

--- calculator.py
+++ calculator.py
@@ -1,11 +1,18 @@
-# we should get user's numbers
+# gathering User Inputs
 
-number1 = input()
-operation = input()
-number2 = input()
+number1 = input("Enter your number: ")
+operation = input("Choose & type an operation: ")
+number2 = input("Enter your second number: ")
 
-number1 = float(number1)
-number2 = float(number2)
+# Data type conversion and Input validation
+try:
+    number1 = float(number1)
+    number2 = float(number2)
+except ValueError :
+    print("Please enter a new number") 
+    exit()
+
+# Conditional Math Operations
 
 if operation == "+":
     print(number1+number2)
@@ -13,7 +20,11 @@ elif operation == "-":
     print(number1-number2)
 elif operation == "*":
     print(number1*number2)
+elif operation == "/":
+    try: 
+        print(number1/number2)
+    #Division by Zero Protection 
+    except ZeroDivisionError:
+        print("Numbers can not be divided by 0!")
 else :
-    print(number1/number2)    
-
-
+    print("Please choose a new operation")
