0bf331767d240bd08bad6b0a6c3a9b19fced1976
EmirhanKARAKOCSME <emirhankarakoc155@gmail.com>
2026-07-22 15:30:37 +0300

    special_calculator_&_math_functions

--- ders1.py
+++ ders1.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+import math
+
+x = 58.698
+#print(math.pi)
+#print(math.e)
+#res = math.sqrt(16)
+#print("The result is:", res)
+#res = math.ceil(x)
+#print("The result is:", res)
+#res = math.floor(x)
+#print("The result is:", res)
---
+++ the_volume_of_sphere.py
@@ -0,0 +1,14 @@
+import math
+
+while True:
+    try:
+        radius = float(input("Enter the radius of the sphere: ")) # get user input for radius
+        if radius < 0:
+            raise ValueError("Radius cannot be negative.") #prompt user to enter a positive number
+    except ValueError as e:
+        print(f"Invalid input! Please enter a valid number: {e}")
+        continue  # Sends the user back to the start of the loop
+    else:
+        volume = 4/3 * math.pi * (radius ** 3) # calculate the volume of the sphere 
+        print("The volume of the sphere is:", volume)
+        break  # Exit the loop after successful calculation
